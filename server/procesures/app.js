2b514bc36340b03fff0830a364e2efaab0bede4b
zhhelical <zzwweepp@gmail.com>
2016-12-19 14:35:41 +0800

    Delete app.js

--- server/procesures/app.js
+++
@@ -1,8 +0,0 @@
-//app.js
-"use strict"
-
-const p_server = require('service/service.js')
-const p_config = require('config.js')
-var server = new p_server.Server()
-server.init(p_config.config.rootPath)
-server.start()
