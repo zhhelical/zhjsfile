a1f43ef8005b88cb93bf40d417336c998c81b7e7
zhhelical <zzwweepp@gmail.com>
2017-01-06 12:41:37 +0800

    Add files via upload

---
+++ txgeo.js
@@ -0,0 +1,119 @@
+//txgeo.js
+var http = require("http"),
+    Url = require("url"),
+    querystring = require('querystring')
+
+/*module.exports = function(location, cb) {
+    $.ajax({
+        type: 'get',
+        url: 'http://apis.map.qq.com/ws/geocoder/v1',
+        dataType: 'jsonp',
+        data: {
+            key: "26SBZ-M6CWI-PDAGZ-5WANA-GSSZS-WCBYN",
+            location: `${location.latitude},${location.longitude}`,
+            get_poi: "1",
+            coord_type: "1",//输入的locations的坐标类型,1 GPS坐标
+            output: "jsonp"
+        },
+        success: function (data, textStatus) {
+            if (data.status == 0) {
+                var address = data.result.formatted_addresses.recommend
+                cb(address)
+            } else {
+                cb("系统错误，请联系管理员！")
+            }
+        },
+        error: function () {
+            cb("系统错误，请联系管理员！")
+        }
+    })
+}
+ function defaults(url){
+ return {
+ // 如果返回false可以取消本次请求
+ beforeSend: function(req){},
+ complete: function(req){},
+ data: '', // Object, String
+ dataType: 'html',
+ error: function(){},
+ headers: {}, // {k:v, ...}
+ statusCode: {},
+ success: function(data){},
+ timeout: 10,
+ type: 'GET', // GET, POST
+ url: url
+ }
+ }*/
+
+
+module.exports = function(location, cb){
+    var settings = {
+        beforeSend: function(req){},
+        complete: function(req){},
+        timeout: 10,
+        type: 'get',
+        url: 'http://apis.map.qq.com/ws/geocoder/v1',
+        dataType: 'jsonp',
+        data: {
+            key: "FKABZ-WWGW2-3LCUP-CZYEL-GFP3Z-KNFYO",
+            location: `${location.latitude},${location.longitude}`,
+            get_poi: "1",
+            coord_type: "1",
+            output: "jsonp"
+        },
+        success: function (data, textStatus) {
+            if (data.status == 0) {
+                var address = data.result.formatted_addresses.recommend
+                cb(address)
+            } else {
+                cb("系统错误，请联系管理员！")
+            }
+        },
+        error: function () {
+            cb("系统错误，请联系管理员！")
+        }
+    }
+
+    var params = Url.parse(settings.url, true)
+
+    var options = {
+        host: params.hostname,
+        port: params.port || 9595,
+        path: params.path,
+        method: settings.type
+    }
+
+    var req = http.request(options, function(res) {
+        var data = ''
+        res.on('data', function(chunk){
+            data += chunk
+        }).on('end', function(){
+            if(	settings.dataType === "json" ){
+                try{
+                    data = JSON.parse(data);
+                }catch(e){
+                    data = null
+                }
+            }
+            //cb(settings.success(data))
+            settings.complete(req)
+        })
+    }).on('error', function(e) {
+        //cb(settings.error(e))
+    })
+
+    if( typeof settings.beforeSend === "function" ){
+        if ( !settings.beforeSend(req) ){
+            settings.complete(req)
+            req.end()
+            return false
+        }
+    }
+
+    if( settings.type === "POST" ){
+        req.write(querystring.stringify(settings.data))
+    }
+
+    req.setTimeout(settings.timeout)
+    req.end()
+}
