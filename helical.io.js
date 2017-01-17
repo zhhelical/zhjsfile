6837405b1b8bd32a7387f42346cb1e34fc010006
zhhelical <zzwweepp@gmail.com>
2017-01-17 11:48:23 +0800

    Delete helical.io.js

--- helical.io.js
+++
@@ -1,7 +0,0 @@
-//helical.io.js
-
-const WxSocketIO = require('./wxsocket.io.js')
-
-var socket = new WxSocketIO()
-socket.connect('http://localhost:9595/node_modules/socket.io/?EIO=3&transport=websocket')
-
