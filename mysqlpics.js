012b53fc4745127dd3d5f56e09829ee5061995db
zhhelical <zzwweepp@gmail.com>
2017-01-18 16:00:32 +0800

    Add files via upload

---
+++ mysqlpics.js
@@ -0,0 +1,50 @@
+//mysqlpics.js
+"use strict"
+var mysql = require('mysql')
+    , co = require('co')
+    , mydb = mysql.createPool({
+            host: 'localhost',
+            user: 'root',
+            password: '635401',
+            database: 'helicaldb'
+        })
+
+module.exports = {
+    insert_pic: function(obj_value) {
+        return new Promise(function (resolve, reject) {
+            mydb.getConnection(function (err, conn) {
+                if (err)
+                    cb(err, null, null)
+                else {
+                    conn.query("INSERT INTO pictures set ?", obj_value, function (err, results) {
+                        conn.release()
+                        if (err) reject(err)
+                        resolve(results)
+                    })
+                }
+            })
+        })
+    },
+    select_pic: function(time, key_value, pos) {
+        return new Promise(function (resolve, reject) {
+            mydb.getConnection(function (err, conn) {
+                if (err)
+                    cb(err, null, null)
+                else {
+                    if (key_value != '') {
+                        mydb.query("SELECT img_name FROM pictures WHERE img_time='" + time + "' AND img_key='" + key_value + "'" + " AND img_pos='" + pos + "'", function (results, err) {
+                            if (err) reject(err)
+                            resolve(results)
+                        })
+                    }
+                    else {
+                        mydb.query("SELECT img_name FROM pictures WHERE img_time='" + time + " AND img_pos='" + pos + "'", function (results, err) {
+                            if (err) reject(err)
+                            resolve(results)
+                        })
+                    }
+                }
+            })
+        })
+    }
+}
