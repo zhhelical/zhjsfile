5f96f5676052c7d709165affa934cab4b973feda
zhhelical <zhhelical@163.com>
2017-03-22 19:43:08 +0800

    js

--- wserver/procesures/sslkeys.js
+++
@@ -1,9 +0,0 @@
-//ssls.js
-"use strict"
-
-const fs = require('fs')
-
-module.exports = {
-    key: fs.readFileSync(process.cwd()+'/2_www.helicalzh.la.key'),
-    cert: fs.readFileSync(process.cwd()+'/1_www.helicalzh.la_bundle.crt')
-}
